8695bdbe6e4db5de88420618034d946a9221f5b8
LisaNota <st107578@student.spbu.ru>
2024-12-19 23:36:33 +0300

    add third experiment

--- metrics.py
+++ metrics.py
@@ -1,40 +1,75 @@
-"""Модуль для оценки результата модели, создания графиков и сохранения метрик"""
-
 import os
+import torch
+import torch.nn as nn
 import pandas as pd
-import csv
-from sklearn.ensemble import RandomForestClassifier
+import pickle
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 import yaml
-import joblib
+import csv
+
 
 with open('params.yaml', 'r') as f:
     params = yaml.safe_load(f)
 exp_name = params['experiment']['name']
 
-model_path = f"experiments/experiment_{exp_name}/model.pkl"
+model_path = f"experiments/experiment_{exp_name}/model.pth"
 output_dir = f"experiments/experiment_{exp_name}"
-os.makedirs(output_dir, exist_ok=True)
 
-data = pd.read_csv('iris.csv')
-
+data = pd.read_csv('iris.csv', index_col=0)
 X_train, X_test, y_train, y_test = train_test_split(
     data.drop('target', axis=1),
     data['target'],
     test_size=params['train']['test_size'],
-    random_state=42
+    random_state=params['train']['random_seed']
 )
 
-model = joblib.load(model_path)
+X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train.values, dtype=torch.long)
+y_test_tensor = torch.tensor(y_test.values, dtype=torch.long)
 
-# Предсказания
-y_pred = model.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
 
-# Сохранение матрицы ошибок
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(4, 128)  # 4 - количество признаков
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)   # 3 — кол-во классов
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+model = MLP().to(device)  # Инициализация модели
+model.load_state_dict(torch.load(model_path))
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+X_test_tensor = X_test_tensor.to(device)
+y_test_tensor = y_test_tensor.to(device)
+
+# Оценка модели
+model.eval()
+with torch.no_grad():
+    y_pred_tensor = model(X_test_tensor)
+    _, y_pred = torch.max(y_pred_tensor, 1)
+
+y_true = y_test_tensor.cpu().numpy()
+y_pred = y_pred.cpu().numpy()
+
+cm = confusion_matrix(y_true, y_pred)
+
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
 plt.title('Confusion Matrix')
@@ -43,9 +78,7 @@ plt.ylabel('True')
 plt.savefig(f"{output_dir}/confusion_matrix.png")
 plt.close()
 
-# Сохранение метрик
-metrics = classification_report(y_test, y_pred, output_dict=True)
-
+metrics = classification_report(y_true, y_pred, output_dict=True)
 flattened_metrics = []
 for label, values in metrics.items():
     if isinstance(values, dict):
@@ -63,6 +96,7 @@ for label, values in metrics.items():
         })
 
 metrics_path = f"{output_dir}/metrics.csv"
+
 with open(metrics_path, 'w', newline='') as f:
     writer = csv.DictWriter(f, fieldnames=['class', 'metric', 'value'])
     writer.writeheader()
--- train.py
+++ train.py
@@ -1,30 +1,77 @@
 import os
-from sklearn.ensemble import RandomForestClassifier
-import joblib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import yaml
-from sklearn.model_selection import train_test_split
+import pickle
 
-# подгружаем параметры
+
 with open('params.yaml', 'r') as f:
     params = yaml.safe_load(f)
 exp_name = params['experiment']['name']
 
-
 output_dir = f"experiments/experiment_{exp_name}"
-os.makedirs(output_dir, exist_ok=True)
-
-data = pd.read_csv('iris.csv')
-X_train, X_test, y_train, y_test = train_test_split(data.drop('target', axis=1),
-                                                    data['target'], test_size=params['train']['test_size'],
-                                                    random_state=42)
+model_path = f"{output_dir}/model.pth"
 
 
-model = RandomForestClassifier(
-    n_estimators=params['train']['n_estimators'],
-    max_depth=params['train']['max_depth'],
-    random_state=42
+data = pd.read_csv('iris.csv', index_col=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    data.drop('target', axis=1),
+    data['target'],
+    test_size=params['train']['test_size'],
+    random_state=params['train']['random_seed']
 )
 
-model.fit(X_train, y_train)
-joblib.dump(model, f"{output_dir}/model.pkl")
+X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train.values, dtype=torch.long)
+y_test_tensor = torch.tensor(y_test.values, dtype=torch.long)
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(4, 128)  # 4 - количество признаков
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 3)   # 3 — кол-во классов
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+# Инициализация модели, функции потерь и оптимизатора
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = MLP().to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=params['train']['learning_rate'])
+loss_function = nn.CrossEntropyLoss()
+
+# Обучение модели
+history = []
+
+for epoch in range(params['train']['num_epochs']):
+    model.train()
+    optimizer.zero_grad()
+
+    inputs = X_train_tensor.to(device)
+    labels = y_train_tensor.to(device)
+
+    outputs = model(inputs)
+    loss = loss_function(outputs, labels)
+
+    loss.backward()
+    optimizer.step()
+
+    # Оценка точности на обучающем наборе
+    _, predicted = torch.max(outputs, 1)
+    correct = (predicted == labels).sum().item()
+    accuracy = correct / labels.size(0)
+    history.append(accuracy)
+
+
+torch.save(model.state_dict(), model_path)
